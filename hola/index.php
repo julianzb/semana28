92627f2f1e62cce6ef955a006ffb7ab3e1f3a9c1
julianzb <70275391+julianzb@users.noreply.github.com>
2020-08-27 12:41:49 -0500

    Add files via upload

---
+++ hola/index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="estilo.css">
+	<title></title>
+</head>
+<body background="2.jpg">
+
+	
+<center><h1 style="font-family: cursive; font-size:40px; ">I N D E P E N D E C I A </h1></center>
+<center><h1 style="font-family: cursive; font-size:40px;">D E </h1></center>
+<center><h1 style="font-family: cursive; font-size:40px;">C O L O M B I A </h1></center>
+
+ <div id="pa">
+   <div id="info">
+     <p id="headline">la independecia</p>
+     <p id="descripcion">La Independencia de Colombia fue el proceso que llevó al final el período de dominio del Imperio español en el actual territorio del país. Dicho proceso se libró en medio de un conflicto desarrollado entre 1810 a 1819 para emancipar los territorios que entonces comprendían el Virreinato de la Nueva Granada.</p>
+   </div>
+ </div>
+
+
+
+
+<center>
+ <div id="pa1">
+   <div id="info1">
+     <p id="headline1">Antecedentes</p>
+     <p id="descripcion1">Las guerras de independencia de la América española se inspiraron en las de Estados Unidos y Haití, así como en la Revolución Francesa y la anterior insurrección de los comuneros. El criollo de Estados Unidos quería la independencia de la dominación inglesa por razones económicas, políticas y sociales.</p>
+   </div>
+ </div>
+</center>
+
+
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+
+
+
+
+
+
+
+
+
+<center>
+ <div id="pa2">
+   <div id="info2">
+     <p id="headline2">¿ por que es importante este dia ?</p>
+     <p id="descripcion2">LEl 20 de julio es uno de los días más importantes en la historia del país ya que en esta fecha se celebra el Día de la Independencia de Colombia, el cual conmemora la firma del Acta de la Revolución en 1810, por lo que en este 2020 se cumplen 210 años desde que el Congreso de Estados Unidos de Colombia</p>
+   </div>
+ </div>
+</center>
+
+
+
+
+</body>
+</html>
